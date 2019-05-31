878598331e8f0316b4c36cf72299e083bf9b0ecd
zhouxindong <zhouxindong@163.com>
2019-05-31 09:14:55 +0800

    fix auto property name

--- cpknife/sugar.h
+++ cpknife/sugar.h
@@ -8,14 +8,14 @@
 
 #pragma region auto_property
 
-#define _DEF_PROPERTY_INIT(type, name, value) type name = value;
-#define _DEF_PROPERTY(type, name) type name;
+#define _DEF_PROPERTY_INIT(type, name, value) type _##name = value;
+#define _DEF_PROPERTY(type, name) type _##name;
 
-#define _GET_PROPERTY(type, name) type get_##name() const {return name;}
-#define _GET_PROPERTY_CONSTREF(type, name) const type & get_##name() const {return name;}
-#define _GET_PROPERTY_REF(type, name) type & get_##name() const {return name;}
+#define _GET_PROPERTY(type, name) type get_##name() const {return _##name;}
+#define _GET_PROPERTY_CONSTREF(type, name) const type & get_##name() const {return _##name;}
+#define _GET_PROPERTY_REF(type, name) type & get_##name() const {return _##name;}
 
-#define _SET_PROPERTY(type, name) void set_##name(const type & value){name = value;}
+#define _SET_PROPERTY(type, name) void set_##name(const type & value){_##name = value;}
 
 #define _PROPERTY(type, name) \
 private:\
